6786e606abc6236464cea4172495a4a4d273c45a
Blacksmith-C <37948495+Blacksmith-C@users.noreply.github.com>
2021-03-22 12:49:58 -0700

    Update COLAPID.cpp
    Reorganized and added comments to de-obfuscate

--- PID controller/COLAPID.cpp
+++ PID controller/COLAPID.cpp
@@ -1,11 +1,33 @@
-#include PID_v1
+#include PID_v1 // This code uses the Arduino PID library. Let's not re-invent the wheel, eh?
 
-void gymbalPID(double inpin = 3, double outpin = 0, double A = 0, double target = 0, double Kp = 1.1, double Ki = 0.1, double Kd = 0.5) {
-	v = analogRead(inpin);
-	PID myPID(v,A,target,Kp,Ki,Kd,DIRECT);
-	myPID.SetMode(AUTOMATIC);
+double colaPIDr() { // Call this function every time you want the roll PID to update. 
+	myPIDr.Compute(); // Run the roll PID update code
+    	return gRoll // The function returns the roll gymbal angle.
+	}
+
+double colaPIDp() { // Call this function every time you want the pitch PID to update. 
+	myPIDp.Compute(); // Run the pitch PID update code
+    	return gPitch // The function returns the pitch gymbal angle.
+	}
+
+void main() { // Below code should be integrated into the main loop program of the Arduino
+	double xdot = 0; // Velocity in the x axis (In practice theses are the velocities (either x or y) that the system derives from sensor data)
+	double ydot = 0; // Velocity in the y axis
+	double target = 0; // Desired velocity value, should always be zero
+	double Kp = 1.1; // Proportional gain
+	double Ki = 0.1; // Integral gain
+	double Kd = 0.5; // Derivative gain
+	double gRoll = 0; // Yaw gymbal angle (In practice these gymbal angle values should be fed into Brandon's conversion code to get a servo angle and then the servos should be driven to that angle)
+	double gPitch = 0; // Pitch gymbal angle
+	
+	PID myPIDr(xdot,gRoll,target,Kp,Ki,Kd,DIRECT); // Initialize roll PID
+	myPIDr.SetMode(AUTOMATIC); // Start roll PID
+	PID myPIDp(ydot,gPitch,target,Kp,Ki,Kd,DIRECT); // Initialize pitch PID
+	myPIDp.SetMode(AUTOMATIC); // Start pitch PID
+	
+	void loop() { // This part of the code repeats until the arduino shuts off or explodes or something
+		gRoll = colaPIDr(); // Update the roll gymbal angle
+		gPitch = colaPIDp(); // Update the pitch gymbal angle
+	}
 }
 
-void loop() {
-	myPID.Compute();
-    	}
