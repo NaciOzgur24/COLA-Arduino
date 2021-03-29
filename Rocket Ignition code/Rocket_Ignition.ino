7dcc203766c56d818b7f99133ceffe41633628f5
NaciOzgur24 <naciozgur1@gmail.com>
2021-03-28 17:30:16 -0700

    Update Rocket_Ignition.ino

--- Rocket Ignition code/Rocket_Ignition.ino
+++ Rocket Ignition code/Rocket_Ignition.ino
@@ -3,11 +3,10 @@ COLA Arduino
 Rocket Ignition code at a given Altitude
 */
 
-#include "GPS_I2C.h" // Gets the Altitude from the GPS sensor
+#include "GPS_I2C.h" // Gets the Altitude from the GPS sensor ***Still NEED to change from .ino file to either .cpp or .h***
 
 int ignite_condition = 0;
-int armed = 0;
-
+// NEED to call variable armed from the GPS_I2C.ino file (i.e. int armed = 0;)
 
 void setup()
 {
@@ -16,14 +15,13 @@ void setup()
 
 void loop()
 {
-    
     while (ignite_condition == 0 && armed == 1)
     {
         if (altitude() /*CHANGE*/ == 11000) // When COLA is 11 meters off the ground
         {
-            digitalWrite(13, HIGH); // sets the digital pin 13 on
-            delay(2000);            // DOUBLE CHECK
-            digitalWrite(13, LOW);  // sets the digital pin 13 off
+            digitalWrite(13, HIGH); // sets the digital pin 13 on (Sends high Voltage to the igniter to light it)
+            delay(2000); // waits 2 seconds after the high voltage is on
+            digitalWrite(13, LOW);  // sets the digital pin 13 off (Turns off the high Voltage)
             ignite_condition = 1;
         }
     }
