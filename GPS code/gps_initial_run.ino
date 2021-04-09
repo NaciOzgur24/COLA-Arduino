ad4698f3a1bcfdcca89c3e4b67a86ce223d96890
NaciOzgur24 <80552650+NaciOzgur24@users.noreply.github.com>
2021-04-09 16:02:01 -0700

    run example code

---
+++ GPS code/gps_initial_run.ino
@@ -0,0 +1,33 @@
+#include <Wire.h> //Needed for I2C to GNSS
+
+#include <SparkFun_u-blox_GNSS_Arduino_Library.h> //http://librarymanager/All#SparkFun_u-blox_GNSS
+SFE_UBLOX_GNSS myGNSS;
+
+long lastTime = 0; //Simple local timer. Limits amount if I2C traffic to u-blox module.
+
+void setup()
+{
+  Serial.begin(115200);
+  while (!Serial); //Wait for user to open terminal
+  Serial.println("SparkFun u-blox Example");
+
+  Wire.begin();
+
+  if (myGNSS.begin() == false) //Connect to the u-blox module using Wire port
+  {
+    Serial.println(F("u-blox GNSS not detected at default I2C address. Please check wiring. Freezing."));
+    while (1);
+  }
+
+  Serial.print(F("Version: "));
+  byte versionHigh = myGNSS.getProtocolVersionHigh();
+  Serial.print(versionHigh);
+  Serial.print(".");
+  byte versionLow = myGNSS.getProtocolVersionLow();
+  Serial.print(versionLow);
+}
+
+void loop()
+{
+  //Do nothing
+}
