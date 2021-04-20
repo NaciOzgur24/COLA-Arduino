9c61696679315d04d05e3958883151113032721d
bdgruber <xbdgruber@gmail.com>
2021-04-19 20:03:46 -0700

    GPS to XYZ Functions

--- Test Plan (ONLY for testing phase code)/Velocity.h
+++ Test Plan (ONLY for testing phase code)/Velocity.h
@@ -7,6 +7,7 @@
 
 void setup()
 {
+	double pi = 3.14159265;
 	//Initial Data
 	long lat1 = (pi / 180) * BLABLA; //[~] Starting Latitude
 	long lon1 = (pi / 180) * BLABLA; //[~] Starting Longitude
@@ -20,49 +21,104 @@ void loop()
 	long altitude = gps_altitude();
 }
 
-double lat_to_x(latitude)
+
+
+
+
+
+
+// INPUT EXAMPLE   -   alt_to_z([Launch Site Ground Elevation (m)], [GPS Altitude OUTPUT])
+
+double alt_to_z(double elevi, double altitude)
 {
-	//Variable initialization for loop code
-	long thetaA = 0, thetaE = 0, lat2 = 0, lon2 = 0, elev2 = 0, dlon = 0, dlat = 0, a = 0, c = 0, d = 0, x = 0, X = 0, Y = 0, Z = 0, Xdot = 0, Ydot = 0, Zdot = 0;
+	double Z = (altitude / 1000) - elevi;
+	return Z;
+}
+
+
+// INPUT EXAMPLE   -   lat_to_x([Reference Latitude]x10^7, [Reference Longitude]x10^7, [GPS Latitude OUTPUT], [GPS Longitude OUTPUT])
+// First two inputs will remain constant. They will be the position of the launch site.
+// OUTPUTS X distance relative to reference position (meters)
+
+double lat_to_x(double lati, double loni, double latitude, double longitude)
+{
+	double pi = 3.14159265;
+	double R = (6378 + 6357) / 2;
+
+	//Initial Data
+	double lat1 = (pi / 180) * lati / 10000000.0; //[~] Starting Latitude
+	double lon1 = (pi / 180) * loni / 10000000.0; //[~] Starting Longitude
 
 	//Current Position data
-	lat2 = (pi / 180) * latitude; //[~] Current Latitude
-	lon2 = (pi / 180) * latitude; //[~] Current Longitude
-	elev2 = 100; //[m] Current elevation in meters
+	double lat2 = (pi / 180) * latitude / 10000000.0; //[~] Current Latitude
+	double lon2 = (pi / 180) * longitude / 10000000.0; //[~] Current Longitude
 
 	//Distance math
-	dlon = lon2 - lon1;
-	dlat = lat2 - lat1;
-	a = (sin(dlat / 2)) ^ 2 + (cos(lat1) * cos(lat2) * (sin(dlon / 2)) ^ 2);
-	c = 2 * asin(min(1, sqrt(a)));
-	d = R * c * 1000; //[m] Lateral distance from stating pt
+	double dlon = (lon2 - lon1);
+	double dlat = (lat2 - lat1);
+	double a = pow((sin(dlat / 2)),2) + (cos(lat1) * cos(lat2) * pow((sin(dlon / 2)),2));
+	double c = 2 * asin(sqrt(a));
+	double d = R * c * 1000; //[m] Lateral distance from stating pt
 
 	//Azimuth Math
-	x = acos((sin(lat2) - sin(lat1) * cos(c)) / (sin(c) * cos(lat1)));
-	if sin(lon2 - lon1) << 0
+	double thetaA = 0;
+	double x = acos((sin(lat2) - sin(lat1) * cos(c)) / (sin(c) * cos(lat1)));
+	if ((sin(lon2 - lon1)) < 0.0)
 	{
 		thetaA = (180 / pi) * x;
 	}
-	else if sin(lon2 - lon1) >> 0
+	else
 	{
 		thetaA = (180 / pi) * (2 * pi - x); //[deg] CCW from due South
 	}
-	//Elevation Math
-	thetaE = (180 / pi) * ((elev2 - elev1) / (d) - (d / 1000) / (2 * R)); //[deg] Up from horizion
-	
-	//Position and velocity out
-	X = d * cos((thetaA - 90) * pi / 180); //[m] East distance from center
-	Y = d * sin((thetaA - 90) * pi / 180); //[m] South distance from center
-	Z = elev2 - elev1;					   //[m] AGL in meters
-	Xdot = X / dt;						   //[m/s] East velocity from center
-	Ydot = Y / dt;						   //[m/s] South velocity from center
-	Zdot = Z / dt;						   //[m/s] Vertical velocity
 
-	double latitude_last = gps_latitude();
-	double longitude_last = gps_longitude();
-	long altitude_last = gps_altitude();
-
-	//return Xdot Ydot Zdot;
+	//Position out
+	double X = d * cos((thetaA - 90) * pi / 180); //[m] East distance from center
+	return X;
 }
+
+
+
+// INPUT EXAMPLE   -   long_to_y([Reference Latitude], [Reference Longitude], [GPS Latitude OUTPUT], [GPS Longitude OUTPUT])
+// First two inputs will remain constant. They will be the position of the launch site.
+// OUTPUTS Y distance relative to reference position (meters)
+
+double long_to_y(double lati, double loni, double latitude, double longitude)
+{
+	double pi = 3.14159265;
+	double R = (6378 + 6357) / 2;
+
+	//Initial Data
+	double lat1 = (pi / 180) * lati / 10000000.0; //[~] Starting Latitude
+	double lon1 = (pi / 180) * loni / 10000000.0; //[~] Starting Longitude
+
+	//Current Position data
+	double lat2 = (pi / 180) * latitude / 10000000.0; //[~] Current Latitude
+	double lon2 = (pi / 180) * longitude / 10000000.0; //[~] Current Longitude
+
+	//Distance math
+	double dlon = (lon2 - lon1);
+	double dlat = (lat2 - lat1);
+	double a = pow((sin(dlat / 2)), 2) + (cos(lat1) * cos(lat2) * pow((sin(dlon / 2)), 2));
+	double c = 2 * asin(sqrt(a));
+	double d = R * c * 1000; //[m] Lateral distance from stating pt
+
+	//Azimuth Math
+	double thetaA = 0;
+	double x = acos((sin(lat2) - sin(lat1) * cos(c)) / (sin(c) * cos(lat1)));
+	if ((sin(lon2 - lon1)) < 0.0)
+	{
+		thetaA = (180 / pi) * x;
+	}
+	else
+	{
+		thetaA = (180 / pi) * (2 * pi - x); //[deg] CCW from due South
+	}
+	//Position out
+	double Y = d * sin((thetaA - 90) * pi / 180); //[m] South distance from center
+	return Y;
+}
+
+
 
 #endif // _VELOCITY_H
