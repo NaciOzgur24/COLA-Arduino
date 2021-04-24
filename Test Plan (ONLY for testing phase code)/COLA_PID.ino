c5c86611bcf145b7a247d89aaecc8a0a540f4593
Blacksmith-C <37948495+Blacksmith-C@users.noreply.github.com>
2021-04-24 15:28:34 -0700

    Create COLA_PID.ino

---
+++ Test Plan (ONLY for testing phase code)/COLA_PID.ino
@@ -0,0 +1,34 @@
+/********************************************************
+ * PID Basic Example
+ * Reading analog input 0 to control analog PWM output 3
+ ********************************************************/
+
+#include <PID_v1.h>
+#include <math.h>
+
+#define PIN_INPUT 0
+#define PIN_OUTPUT 3
+
+//Define Variables we'll be connecting to
+double Setpoint, Input, Output;
+
+//Specify the links and initial tuning parameters
+double Kp=1.1, Ki=0.1, Kd=0.5;
+PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
+
+void setup()
+{
+  //initialize the variables we're linked to
+  Input = analogRead(PIN_INPUT);
+  Setpoint = 100;
+
+  //turn the PID on
+  myPID.SetMode(AUTOMATIC);
+}
+
+void loop()
+{
+  Input = analogRead(PIN_INPUT);
+  myPID.Compute();
+  analogWrite(PIN_OUTPUT, Output);
+}
