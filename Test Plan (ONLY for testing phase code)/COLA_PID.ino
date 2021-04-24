222ebed1f323a3dfc1b49ec3e7ce36110f35fdb7
Blacksmith-C <37948495+Blacksmith-C@users.noreply.github.com>
2021-04-24 15:34:46 -0700

    Update COLA_PID.ino

--- Test Plan (ONLY for testing phase code)/COLA_PID.ino
+++ Test Plan (ONLY for testing phase code)/COLA_PID.ino
@@ -1,34 +1,25 @@
-/********************************************************
- * PID Basic Example
- * Reading analog input 0 to control analog PWM output 3
- ********************************************************/
-
 #include <PID_v1.h>
 #include <math.h>
 
 #define PIN_INPUT 0
 #define PIN_OUTPUT 3
 
-//Define Variables we'll be connecting to
 double Setpoint, Pitch, gPitch;
 
-//Specify the links and initial tuning parameters
 double Kp=1.1, Ki=0.1, Kd=0.5;
 PID myPIDp(&Pitch, &gPitch, &Setpoint, Kp, Ki, Kd, DIRECT);
 
 void setup()
 {
-  //initialize the variables we're linked to
   Pitch = 15; //Arbitrary Start Point
   Setpoint = 0; //Drive towards zero pitch
 
-  //turn the PID on
-  myPIDp.SetMode(AUTOMATIC);
+  myPIDp.SetMode(AUTOMATIC); //start PID
 }
 
 void loop()
 {
-  //Input = get input
+  //get input
   myPIDp.Compute();
   //send output to gymbal servo code
 }
